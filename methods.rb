9091beeb378ea1ebe0b1bc2339e9f8399772300b
Steve <steve.maina@student.moringaschool.com>
2022-10-17 14:59:20 +0300

    Complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts ("Hello, programmer!")
+end
+
+greet_programmer
+def greet(name="Naureen",name_2="Jimmy")
+    puts"Hello, #{name}!"
+    puts"Hello, #{name_2}"
+    end
+    greet
+ 
+
+def greet_with_default name="Naureen"
+    puts "Hello, #{name}!"
+    end
+    greet_with_default
+    def greet_with_default name="programmer"
+        puts "Hello, #{name}!"
+        end
+        greet_with_default
+
+        def add num1=2,num2=5
+            return num1 + num2
+            end
+            add
+       def halve num=7
+        if num.class != Integer
+            return nil
+          end
+        return num/2
+        end
+        halve
+
+     
+     
